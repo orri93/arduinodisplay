eb7e668dc391ff0d58d4f7f75689f833f5ad9708
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2019-07-28 14:59:57 +0200

    Update

--- src/arduinodisplay.cpp
+++ src/arduinodisplay.cpp
@@ -10,21 +10,13 @@
 namespace fds {
 namespace display {
 
-
 U8G2_SSD1306_128X32_UNIVISION_1_HW_I2C u8g2(U8G2_R0);
 
 static bool isdisplaystarting = false;
 static uint8_t todisplay = 0;
 static char* formated;
 
-void twolines() {
-  u8g2.setFont(DISPLAY_FONT_TWO_LINES);
-  formated = fds::format::get(0);
-  u8g2.drawStr(0, DISPLAY_TWO_LINES_Y1, formated);
-  u8g2.setFont(DISPLAY_FONT_TWO_LINES);
-  formated = fds::format::get(1);
-  u8g2.drawStr(0, DISPLAY_TWO_LINES_Y2, formated);
-}
+static void twolines();
 
 void logo(u8g2_uint_t w, u8g2_uint_t h, const uint8_t *bitmap) {
   // picture loop
@@ -61,6 +53,15 @@ void loop() {
       todisplay = 0;
     }
   }
+}
+
+void twolines() {
+  u8g2.setFont(DISPLAY_FONT_TWO_LINES);
+  formated = fds::format::get(0);
+  u8g2.drawStr(0, DISPLAY_TWO_LINES_Y1, formated);
+  u8g2.setFont(DISPLAY_FONT_TWO_LINES);
+  formated = fds::format::get(1);
+  u8g2.drawStr(0, DISPLAY_TWO_LINES_Y2, formated);
 }
 
 }
